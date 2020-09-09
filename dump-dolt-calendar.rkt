049156aa72fa5b320bf48efdfbe19ce1b92c1cd4
evdubs <1124030+evdubs@users.noreply.github.com>
2020-09-09 11:53:06 -1000

    add dump dolt programs

---
+++ dump-dolt-calendar.rkt
@@ -0,0 +1,66 @@
+#lang racket/base
+
+(require db
+         gregor
+         racket/cmdline
+         racket/string
+         racket/system)
+
+(define base-folder (make-parameter "/var/tmp/dolt/calendar-estimates-statements"))
+
+(define as-of-date (make-parameter (~t (today) "yyyy-MM-dd")))
+
+(define db-user (make-parameter "user"))
+
+(define db-name (make-parameter "local"))
+
+(define db-pass (make-parameter ""))
+
+(command-line
+ #:program "racket dump-dolt-calendar.rkt"
+ #:once-each
+ [("-b" "--base-folder") folder
+                         "Base dolt folder. Defaults to /var/tmp/dolt/calendar-estimates-statements"
+                         (base-folder folder)]
+ [("-d" "--date") date
+                  "Final date for history retrieval. Defaults to today"
+                  (as-of-date date)]
+ [("-n" "--db-name") name
+                     "Database name. Defaults to 'local'"
+                     (db-name name)]
+ [("-p" "--db-pass") password
+                     "Database password"
+                     (db-pass password)]
+ [("-u" "--db-user") user
+                     "Database user name. Defaults to 'user'"
+                     (db-user user)])
+
+(define dbc (postgresql-connect #:user (db-user) #:database (db-name) #:password (db-pass)))
+
+; earnings-calendar
+(system (string-append "cd " (base-folder) "; /usr/local/bin/dolt sql -q \"delete from earnings_calendar where date >= '" (as-of-date) "'\";"))
+
+(define earnings-calendar-file (string-append (base-folder) "/earnings-calendar-" (as-of-date) ".csv"))
+
+(call-with-output-file earnings-calendar-file
+  (λ (out)
+    (displayln "act_symbol,date,when" out)
+    (for-each (λ (row)
+                (displayln (string-join (vector->list row) ",") out))
+              (query-rows dbc "
+select
+  act_symbol::text,
+  date::text,
+  coalesce(\"when\"::text, '')
+from
+  zacks.earnings_calendar
+where
+  date >= $1::text::date
+"
+                          (as-of-date))))
+  #:exists 'replace)
+
+(system (string-append "cd " (base-folder) "; /usr/local/bin/dolt table import -u earnings_calendar earnings-calendar-" (as-of-date) ".csv"))
+
+(system (string-append "cd " (base-folder) "; /usr/local/bin/dolt add earnings_calendar; "
+                       "/usr/local/bin/dolt commit -m 'earnings_calendar " (as-of-date) " update'; /usr/local/bin/dolt push"))
---
+++ dump-dolt-estimates.rkt
@@ -0,0 +1,180 @@
+#lang racket/base
+
+(require db
+         gregor
+         racket/cmdline
+         racket/string
+         racket/system)
+
+(define base-folder (make-parameter "/var/tmp/dolt/calendar-estimates-statements"))
+
+(define start-date (make-parameter (~t (today) "yyyy-MM-dd")))
+
+(define end-date (make-parameter (~t (today) "yyyy-MM-dd")))
+
+(define db-user (make-parameter "user"))
+
+(define db-name (make-parameter "local"))
+
+(define db-pass (make-parameter ""))
+
+(command-line
+ #:program "racket dump-dolt-estimates.rkt"
+ #:once-each
+ [("-b" "--base-folder") folder
+                         "Base dolt folder. Defaults to /var/tmp/dolt/calendar-estimates-statements"
+                         (base-folder folder)]
+ [("-e" "--end-date") end
+                      "Final date for history retrieval. Defaults to today"
+                      (end-date end)]
+ [("-n" "--db-name") name
+                     "Database name. Defaults to 'local'"
+                     (db-name name)]
+ [("-p" "--db-pass") password
+                     "Database password"
+                     (db-pass password)]
+ [("-s" "--start-date") start
+                        "Earliest date for history retrieval. Defaults to today"
+                        (start-date start)]
+ [("-u" "--db-user") user
+                     "Database user name. Defaults to 'user'"
+                     (db-user user)])
+
+(define dbc (postgresql-connect #:user (db-user) #:database (db-name) #:password (db-pass)))
+
+; rank-score
+(for-each (λ (date)
+            (define rank-score-file (string-append (base-folder) "/rank-score-" date ".csv"))
+            (call-with-output-file rank-score-file
+              (λ (out)
+                (displayln "act_symbol,date,rank,value,growth,momentum,vgm" out)
+                (for-each (λ (row)
+                            (displayln (string-join (vector->list row) ",") out))
+                          (query-rows dbc "
+select
+  act_symbol::text,
+  date::text,
+  rank::text,
+  value::text,
+  growth::text,
+  momentum::text,
+  vgm::text
+from
+  zacks.rank_score
+where
+  date = $1::text::date
+order by
+  act_symbol, date;
+"
+                                      date)))
+              #:exists 'replace)
+            (system (string-append "cd " (base-folder) "; /usr/local/bin/dolt table import -u rank_score rank-score-" date ".csv")))
+          (query-list dbc "
+select distinct
+  date::text
+from
+  zacks.rank_score
+where
+  date >= $1::text::date and
+  date <= $2::text::date
+order by
+  date;
+"
+                      (start-date)
+                      (end-date)))
+
+(system (string-append "cd " (base-folder) "; /usr/local/bin/dolt add rank_score; "
+                       "/usr/local/bin/dolt commit -m 'rank_score " (end-date) " update'; /usr/local/bin/dolt push"))
+
+; eps-estimate
+(for-each (λ (date)
+            (define eps-estimate-file (string-append (base-folder) "/eps-estimate-" date ".csv"))
+            (call-with-output-file eps-estimate-file
+              (λ (out)
+                (displayln "act_symbol,date,period,period_end_date,consensus,recent,count,high,low,year_ago" out)
+                (for-each (λ (row)
+                            (displayln (string-join (vector->list row) ",") out))
+                          (query-rows dbc "
+select
+  act_symbol::text,
+  date::text,
+  period::text,
+  period_end_date::text,
+  coalesce(consensus::text, ''),
+  coalesce(recent::text, ''),
+  coalesce(count::text, ''),
+  coalesce(high::text, ''),
+  coalesce(low::text, ''),
+  coalesce(year_ago::text, '')
+from
+  zacks.eps_estimate
+where
+  date = $1::text::date
+order by
+  act_symbol, date, period;
+"
+                                      date)))
+              #:exists 'replace)
+            (system (string-append "cd " (base-folder) "; /usr/local/bin/dolt table import -u eps_estimate eps-estimate-" date ".csv")))
+          (query-list dbc "
+select distinct
+  date::text
+from
+  zacks.eps_estimate
+where
+  date >= $1::text::date and
+  date <= $2::text::date
+order by
+  date;
+"
+                      (start-date)
+                      (end-date)))
+
+(system (string-append "cd " (base-folder) "; /usr/local/bin/dolt add eps_estimate; "
+                       "/usr/local/bin/dolt commit -m 'eps_estimate " (end-date) " update'; /usr/local/bin/dolt push"))
+
+; sales-estimate
+(for-each (λ (date)
+            (define sales-estimate-file (string-append (base-folder) "/sales-estimate-" date ".csv"))
+            (call-with-output-file sales-estimate-file
+              (λ (out)
+                (displayln "act_symbol,date,period,period_end_date,consensus,count,high,low,year_ago" out)
+                (for-each (λ (row)
+                            (displayln (string-join (vector->list row) ",") out))
+                          (query-rows dbc "
+select
+  act_symbol::text,
+  date::text,
+  period::text,
+  period_end_date::text,
+  coalesce(consensus::text, ''),
+  coalesce(count::text, ''),
+  coalesce(high::text, ''),
+  coalesce(low::text, ''),
+  coalesce(year_ago::text, '')
+from
+  zacks.sales_estimate
+where
+  date = $1::text::date
+order by
+  act_symbol, date, period;
+"
+                                      date)))
+              #:exists 'replace)
+            (system (string-append "cd " (base-folder) "; /usr/local/bin/dolt table import -u sales_estimate sales-estimate-" date ".csv")))
+          (query-list dbc "
+select distinct
+  date::text
+from
+  zacks.sales_estimate
+where
+  date >= $1::text::date and
+  date <= $2::text::date
+order by
+  date;
+"
+                      (start-date)
+                      (end-date)))
+
+(system (string-append "cd " (base-folder) "; /usr/local/bin/dolt add sales_estimate; "
+                       "/usr/local/bin/dolt commit -m 'sales_estimate " (end-date) " update'; /usr/local/bin/dolt push"))
