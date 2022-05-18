07d4c7e27a966cc67bc6191017cff9eba149554b
evdubs <1124030+evdubs@users.noreply.github.com>
2022-05-18 09:34:27 -1000

    use '-y' to auto recompile racket files

--- estimate-extract-transform-load.sh
+++ estimate-extract-transform-load.sh
@@ -3,9 +3,9 @@
 today=$(date "+%F")
 dir=$(dirname "$0")
 
-racket ${dir}/estimate-extract.rkt -p "$1"
-racket ${dir}/estimate-transform-load.rkt -p "$1"
+racket -y ${dir}/estimate-extract.rkt -p "$1"
+racket -y ${dir}/estimate-transform-load.rkt -p "$1"
 
 7zr a /var/tmp/zacks/estimates/${today}.7z /var/tmp/zacks/estimates/${today}/*.html
 
-racket ${dir}/dump-dolt-estimates.rkt -p "$1"
+racket -y ${dir}/dump-dolt-estimates.rkt -p "$1"
